79d9818b9808b65c52cd3468704522f412c4226d
an-nguyen <annguyen@anduintransact.com>
2025-01-21 15:50:38 +0700

    sample

--- setup.py
+++
@@ -1,6 +0,0 @@
-from setuptools import setup
-from Cython.Build import cythonize
-
-setup(
-    ext_modules = cythonize("src/imppkg/harmonic_mean.pyx")
-)
--- src/imppkg/harmonic_mean.pyx
+++
@@ -1,4 +0,0 @@
-
-def harmonic_mean(nums: list[float]):
-    H = len(nums)/ sum([1/num for num in nums])
-    return H 
